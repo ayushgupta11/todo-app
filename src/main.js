9f7e02801bd7faa52620b306158268901c4a54d3
Ayush Gupta <ayush.gupta.1197@gmail.com>
2018-09-15 14:03:10 +0530

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
 import './registerServiceWorker'
+
+import './styles/quasar.styl'
+import 'quasar-extras/animate'
+import 'quasar-extras/material-icons'
+import 'quasar-extras/fontawesome'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
 
 Vue.config.productionTip = false
 
